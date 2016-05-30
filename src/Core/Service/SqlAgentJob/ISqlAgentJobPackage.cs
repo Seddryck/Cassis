739c649365759979982f7d7534dd5474d9990d62
Cédric L. Charlier <cedric.charlier@yucom.be>
2016-05-30 21:17:07 +0200

    Start a SqlAgent job

---
+++ src/Core/Service/SqlAgentJob/ISqlAgentJobPackage.cs
@@ -0,0 +1,18 @@
+﻿#if ! SqlServer2008R2
+using System;
+using System.Linq;
+using System.Xml.Serialization;
+using System.ComponentModel;
+
+namespace Cassis.Core.Service.SqlAgentJob
+{
+    [XmlRootAttribute("package-sql-agent-job")]
+    public interface ISqlAgentJobPackage : IPackageInfo
+    {
+        [XmlAttribute("server")]
+        string Server { get; set; }
+        [XmlAttribute("job")]
+        string JobName { get; set; }
+    }
+}
+#endif
---
+++ src/Core/Service/SqlAgentJob/SqlAgentJobService.cs
@@ -0,0 +1,66 @@
+﻿#if ! SqlServer2008R2
+using Cassis.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Cassis.Core.Service.Catalog
+{
+    class SqlAgentService : AbstractPackageService
+    {
+        public new ISqlAgentPackage PackageInfo
+        {
+            get { return base.PackageInfo as ISqlAgentPackage; }
+        }
+
+        public SqlAgentService(ISqlAgentPackage packageInfo)
+            : base(packageInfo)
+        { }
+
+        public override PackageResponse Run()
+        {
+            return Run(PackageInfo);
+        }
+
+        protected PackageResponse Run(ISqlAgentPackage etl)
+        {
+            var connection = new SqlConnection(string.Format(@"Data Source={0};Initial Catalog=msdb;Integrated Security=SSPI;", etl.Server));
+            var command = new SqlCommand("sp_start_job", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            var returnValue = new SqlParameter("@RETURN_VALUE", SqlDbType.Int);
+            returnValue.Direction = ParameterDirection.ReturnValue;
+            command.Parameters.Add(returnValue);
+
+            var parameter = new SqlParameter("@job_name", SqlDbType.VarChar);
+            parameter.Direction = ParameterDirection.Input;
+            command.Parameters.Add(parameter);
+            parameter.Value = etl.JobName;
+
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+                var result = (Int32)command.Parameters["@RETURN_VALUE"].Value;
+                return new PackageResponse(result == 0);
+            }
+            catch (Exception ex)
+            {
+                var packageEvent = new PackageEvents();
+                packageEvent.Errors.Add(ex.Message);
+                if (ex.InnerException!=null)
+                    packageEvent.Errors.Add(ex.InnerException.Message);
+                return new PackageResponse(false, packageEvent);
+            }
+            finally
+            {
+                connection.Close();
+            }   
+        }
+    }
+}
+#endif
